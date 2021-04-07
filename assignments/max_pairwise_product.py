3848dbe696e52ddf4f33e1f5f77d40afc8ffb2d0
DumbledoreD <dumbledoredeng@gmail.com>
2021-04-07 02:41:30 +0000

    Add c1w1 assignment solution

---
+++ assignments/max_pairwise_product.py
@@ -0,0 +1,19 @@
+# python3
+
+
+def max_pairwise_product(numbers):
+    a = b = 0
+
+    for number in numbers:
+        if number >= a:
+            b, a = a, number
+        elif number > b:
+            b = number
+
+    return a * b
+
+
+if __name__ == "__main__":
+    input_n = int(input())
+    input_numbers = [int(x) for x in input().split()]
+    print(max_pairwise_product(input_numbers))
