688a973fe40c2c093ff57e2b8e2a2324fee8d0af
DumbledoreD <dumbledoredeng@gmail.com>
2021-04-08 14:50:18 +0000

    Solve lcm

---
+++ assignments/lcm.py
@@ -0,0 +1,17 @@
+# Uses python3
+
+
+def lcm(a, b):
+    return int(a * b / gcd(a, b))
+
+
+def gcd(a, b):
+    if b == 0:
+        return a
+
+    return gcd(b, a % b)
+
+
+if __name__ == "__main__":
+    a, b = map(int, input().split())
+    print(lcm(a, b))
