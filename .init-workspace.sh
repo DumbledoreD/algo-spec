8faaf68a146b405054d487382241131b61435669
DumbledoreD <dumbledoredeng@gmail.com>
2021-04-08 13:24:14 +0000

    Tweak workspace setup

--- .init-workspace.sh
+++ .init-workspace.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # Set up pre-commit
-python3.8 -m pip install --user pre-commit
+python3 -m pip install --user pre-commit
 pre-commit install
 pre-commit autoupdate
+
+# Pytest
+python3 -m pip install --user pytest
