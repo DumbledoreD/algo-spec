dba5687dadaed50e4348cc59c1a94dee89247554
DumbledoreD <dumbledoredeng@gmail.com>
2021-05-03 00:24:42 +0000

    Solve is bst

---
+++ assignments/c2w6/is_bst.py
@@ -0,0 +1,32 @@
+import sys
+import threading
+
+sys.setrecursionlimit(10 ** 7)  # max depth of recursion
+threading.stack_size(2 ** 27)  # new thread will get stack of such size
+
+
+def is_bst(node_index, min_key, max_key, tree):
+    if not tree or node_index == -1:
+        return True
+
+    key, left_child_index, right_child_index = tree[node_index]
+
+    if not (min_key < key < max_key):
+        return False
+
+    return is_bst(left_child_index, min_key, key, tree) and is_bst(
+        right_child_index, key, max_key, tree
+    )
+
+
+def main():
+    nodes = int(sys.stdin.readline().strip())
+    tree = []
+    for _ in range(nodes):
+        tree.append(list(map(int, sys.stdin.readline().strip().split())))
+
+    print("CORRECT" if is_bst(0, -float("inf"), float("inf"), tree) else "INCORRECT")
+
+
+if __name__ == "__main__":
+    threading.Thread(target=main).start()
