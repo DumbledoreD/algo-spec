dc9f409f8b2b1d30bbb1998250e8ddc26ac512ff
DumbledoreD <dumbledoredeng@gmail.com>
2021-05-15 02:10:05 +0000

    Solve suffix array

---
+++ assignments/c4w4/suffix_array_long_test.py
@@ -0,0 +1,25 @@
+import random
+
+import pytest
+
+from .suffix_array_long import ALPHABET
+from .suffix_array_long import build_suffix_array as fast
+
+TRUE_ALPHABET = ALPHABET[1:]
+
+
+def slow(text):
+    suffixes = [(text[i:], i) for i in range(len(text))]
+    suffixes.sort()
+    return [start for _, start in suffixes]
+
+
+@pytest.mark.parametrize(
+    "text",
+    [
+        "".join([random.choice(TRUE_ALPHABET) for char in range(50)] + ["$"])
+        for trial in range(10000)
+    ],
+)
+def test_basic(text):
+    assert fast(text) == slow(text)
