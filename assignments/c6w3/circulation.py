6c2b2864cfedb456526d006cd743d171b71e8f75
DumbledoreD <dumbledoredeng@gmail.com>
2021-07-24 20:27:55 +0000

    Solve circulation

--- assignments/c6w3/circulation.py
+++ assignments/c6w3/circulation.py
@@ -1,6 +1,6 @@
 import sys
-from collections import deque
 from enum import Enum
+from itertools import chain
 
 
 class FlowDirection(Enum):
@@ -8,25 +8,31 @@ class FlowDirection(Enum):
     Backward = 1
 
 
-class EdmondsKarpWithLowerBound:
+class FordFulkersonWithLowerBound:
     def __init__(self, n, edges):
         graph = GraphBuilder(n, edges)
         self._vertices, self._edges = graph.vertices, graph.edges
 
         self._remove_lower_bound()
-        self._remove_demand()
-        self._do_edmonds_karp()
+        self._add_super_nodes()
+        self._do_ford_fulkerson()
         self._restore_lower_bound()
 
     def _remove_lower_bound(self):
         for edge in self._edges:
-            edge.capacity -= edge.lb
+            edge.ub -= edge.lb
+
+            # dv' = dv - (Lin - Lout)
+            # dv - original vertex demand
+            # Lin - sum of vertex input edges lower bounds
+            # Lout - sum of vertex output edges lower bounds
             edge.fr.demand += edge.lb
             edge.to.demand -= edge.lb
 
-    def _remove_demand(self):
+    def _add_super_nodes(self):
         self._super_source = Vertex("source")
         self._super_sink = Vertex("sink")
+        self._total_demand = 0
 
         for vertex in self._vertices:
             if vertex.demand < 0:
@@ -36,10 +42,13 @@ class EdmondsKarpWithLowerBound:
 
             elif vertex.demand > 0:
                 vertex.outs.append(Edge(0, vertex.demand, vertex, self._super_sink))
+                self._total_demand += vertex.demand
 
-    def _do_edmonds_karp(self):
+    def _do_ford_fulkerson(self):
+        self._max_flow = 0
+
         while True:
-            path = self._bfs()
+            path = self._dfs()
 
             if not path:
                 break
@@ -49,7 +58,7 @@ class EdmondsKarpWithLowerBound:
 
             for direction, edge in path:
                 if direction == FlowDirection.Forward:
-                    path_flow = min(path_flow, edge.capacity - edge.flow)
+                    path_flow = min(path_flow, edge.ub - edge.flow)
 
                 elif direction == FlowDirection.Backward:
                     path_flow = min(path_flow, edge.flow)
@@ -62,55 +71,51 @@ class EdmondsKarpWithLowerBound:
                 elif direction == FlowDirection.Backward:
                     edge.flow -= path_flow
 
-    def _bfs(self):
+            self._max_flow += path_flow
+
+    def _dfs(self):
         seen = set()
         seen.add(self._super_source)
 
-        queue = deque([(self._super_source, [])])
+        stack = [(self._super_source, [])]
 
-        while queue:
-            u, path = queue.popleft()
+        while stack:
+            u, path = stack.pop()
 
-            for edge in u.outs:
-                direction = (
-                    FlowDirection.Forward if edge.fr == u else FlowDirection.Backward
-                )
+            for edge in chain(u.outs, u.ins):
+                if edge.fr == u and edge.flow < edge.ub:
+                    direction = FlowDirection.Forward
+                    next_node = edge.to
 
-                if (
-                    (edge.to in seen)
-                    or (
-                        direction == FlowDirection.Forward
-                        and edge.flow == edge.capacity
-                    )
-                    or (direction == FlowDirection.Backward and edge.flow == 0)
-                ):
+                elif edge.to == u and edge.flow != 0:
+                    direction = FlowDirection.Backward
+                    next_node = edge.fr
+
+                else:
+                    continue
+
+                if next_node in seen:
                     continue
 
                 new_path = path + [(direction, edge)]
 
-                if edge.to == self._super_sink:
+                if next_node == self._super_sink:
                     return new_path
 
-                else:
-                    queue.append((edge.to, new_path))
-                    seen.add(edge.to)
+                stack.append((next_node, new_path))
+                seen.add(next_node)
 
     def _restore_lower_bound(self):
         for edge in self._edges:
             edge.flow += edge.lb
+            edge.ub += edge.lb
 
             edge.fr.demand -= edge.lb
             edge.to.demand += edge.lb
 
-            edge.fr.net_flow -= edge.flow
-            edge.to.net_flow += edge.flow
-
     @property
     def solvable(self):
-        for v in self._vertices:
-            if v.net_flow != 0:
-                return False
-        return True
+        return self._max_flow == self._total_demand
 
     @property
     def edges(self):
@@ -139,16 +144,9 @@ class GraphBuilder:
             v = self._vertices[v]
 
             e = Edge(lb, c, u, v)
-            u.outs.append(e)
-            v.outs.append(e)  # Add to v.outs as backward edge
+            u.outs.append(e)  # Forward edge
+            v.ins.append(e)  # Backward edge
             self._edges.append(e)
-
-            # dv' = dv - (Lin - Lout)
-            # dv - original vertex demand
-            # Lin - sum of vertex input edges lower bounds
-            # Lout - sum of vertex output edges lower bounds
-            u.demand += lb
-            v.demand -= lb
 
     @property
     def vertices(self):
@@ -162,42 +160,35 @@ class GraphBuilder:
 class Vertex:
     def __init__(self, key):
         self.key = key
-        self.net_flow = 0
         self.demand = 0
         self.outs = []
+        self.ins = []
 
     def __repr__(self):
-        return "Vertex(key={}, net_flow={}, demand={}, outs={})".format(
-            self.key, self.net_flow, self.demand, [e.to.key for e in self.outs]
+        return "Vertex(key={}, demand={}, outs={}, ins={})".format(
+            self.key,
+            self.demand,
+            [e.to.key for e in self.outs],
+            [e.fr.key for e in self.ins],
         )
 
 
 class Edge:
-    def __init__(self, lb, capacity, fr, to):
+    def __init__(self, lb, ub, fr, to):
         self.flow = 0
-        self.lb, self.capacity, self.fr, self.to = lb, capacity, fr, to
+        self.lb, self.ub, self.fr, self.to = lb, ub, fr, to
 
     def __repr__(self):
-        return "Edge(flow={}, lb={}, capacity={}, fr={}, to={})".format(
-            self.flow, self.lb, self.capacity, self.fr.key, self.to.key
+        return "Edge(flow={}, lb={}, ub={}, fr={}, to={})".format(
+            self.flow, self.lb, self.ub, self.fr.key, self.to.key
         )
 
 
 def main():
-    (
-        nm,
-        *edges,
-    ) = """\
-3 3
-1 2 1 3
-2 3 2 4
-3 1 1 2
-""".split(
-        "\n"
-    )
+    (nm, *edges) = sys.stdin.read().split("\n")
     n, m = map(int, nm.split())
 
-    solver = EdmondsKarpWithLowerBound(n, edges)
+    solver = FordFulkersonWithLowerBound(n, edges)
 
     if solver.solvable:
         print("YES")
